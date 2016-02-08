8909f6ec01acdb1e3b9dec23e5992182395cdbda
DavidGalvanDom <davigalva@hotmail.com>
2016-02-08 17:11:45 -0600

    Ordenar el menu

--- BasedeDatos/Scripts CMI-Track.sql
+++ BasedeDatos/Scripts CMI-Track.sql
@@ -7,27 +7,29 @@ Insert into cmiMenuGrupo VALUES (6,'Embarque','fa-truck',getdate(),getdate(),1,1
 
 select * from cmiModulos
 
+
+
 INSERT INTO cmiModulos Values (1,getdate(), getdate(),1,'Usuario','Usuario/Index',1)
 INSERT INTO cmiModulos Values (2,getdate(), getdate(),1,'Departamento','Departamento/Index',2)
 INSERT INTO cmiModulos Values (3,getdate(), getdate(),1,'Permisos','',3)
 
-INSERT INTO cmiModulos Values (4,getdate(), getdate(),1,'Tipo Construccion','TipoConstruccion/Index',1)
-INSERT INTO cmiModulos Values (5,getdate(), getdate(),1,'Categoria','Categoria/Index',2)
-INSERT INTO cmiModulos Values (6,getdate(), getdate(),1,'Tipo Proceso','TipoProceso/Index',3)
-INSERT INTO cmiModulos Values (7,getdate(), getdate(),1,'Procesos','Procesos/Index',4)
-INSERT INTO cmiModulos Values (8,getdate(), getdate(),1,'Rutas de Fabricacion','RutasdeFabricacion/Index',5)
-INSERT INTO cmiModulos Values (9,getdate(), getdate(),1,'Tipo Material','TipoMaterial/Index',6)
-INSERT INTO cmiModulos Values (10,getdate(), getdate(),1,'Unidad de Medida','UnidadMedida/Index',7)
-INSERT INTO cmiModulos Values (11,getdate(), getdate(),1,'Calidad','Calidad/Index',8)
-INSERT INTO cmiModulos Values (12,getdate(), getdate(),1,'Calidad-Proceso','CalidadProceso/Index',9)
-INSERT INTO cmiModulos Values (13,getdate(), getdate(),1,'Grupos Material','Grupo/Index',10)
-INSERT INTO cmiModulos Values (14,getdate(), getdate(),1,'Almacen','Almacen/Index',11)
-INSERT INTO cmiModulos Values (15,getdate(), getdate(),1,'Materiales','Materiales/Index',12)
-INSERT INTO cmiModulos Values (16,getdate(), getdate(),1,'Tipos Movtos Mat','TipoMovtoMaterial/Index',13)
-INSERT INTO cmiModulos Values (17,getdate(), getdate(),1,'Clientes','Cliente/Index',14)
-INSERT INTO cmiModulos Values (18,getdate(), getdate(),1,'Origen Requisicion','OrigenRequisicion/Index',15)
+INSERT INTO cmiModulos Values (4,getdate(), getdate(),1,'Tipo Construccion','TipoConstruccion/Index',11)
+INSERT INTO cmiModulos Values (5,getdate(), getdate(),1,'Categoria','Categoria/Index',4)
+INSERT INTO cmiModulos Values (6,getdate(), getdate(),1,'Tipo Proceso','TipoProceso/Index',12)
+INSERT INTO cmiModulos Values (7,getdate(), getdate(),1,'Procesos','Procesos/Index',9)
+INSERT INTO cmiModulos Values (8,getdate(), getdate(),1,'Rutas de Fabricacion','RutasdeFabricacion/Index',10)
+INSERT INTO cmiModulos Values (9,getdate(), getdate(),1,'Tipo Material','TipoMaterial/Index',13)
+INSERT INTO cmiModulos Values (10,getdate(), getdate(),1,'Unidad de Medida','UnidadMedida/Index',15)
+INSERT INTO cmiModulos Values (11,getdate(), getdate(),1,'Calidad','Calidad/Index',2)
+INSERT INTO cmiModulos Values (12,getdate(), getdate(),1,'Calidad-Proceso','CalidadProceso/Index',3)
+INSERT INTO cmiModulos Values (13,getdate(), getdate(),1,'Grupos Material','Grupo/Index',6)
+INSERT INTO cmiModulos Values (14,getdate(), getdate(),1,'Almacen','Almacen/Index',1)
+INSERT INTO cmiModulos Values (15,getdate(), getdate(),1,'Materiales','Materiales/Index',7)
+INSERT INTO cmiModulos Values (16,getdate(), getdate(),1,'Tipos Movtos Mat','TipoMovtoMaterial/Index',14)
+INSERT INTO cmiModulos Values (17,getdate(), getdate(),1,'Clientes','Cliente/Index',5)
+INSERT INTO cmiModulos Values (18,getdate(), getdate(),1,'Origen Requisicion','OrigenRequisicion/Index',8)
 
-
+INSERT INTO cmiModulos Values (20,getdate(), getdate(),1,'Proyecto','Proyecto/Index',1)
 
 INSERT INTO cmiModuloMenuGrupo values (1,1)
 INSERT INTO cmiModuloMenuGrupo values (1,2)
@@ -47,6 +49,7 @@ INSERT INTO cmiModuloMenuGrupo values (2,15)
 INSERT INTO cmiModuloMenuGrupo values (2,16)
 INSERT INTO cmiModuloMenuGrupo values (2,17)
 INSERT INTO cmiModuloMenuGrupo values (2,18)
+INSERT INTO cmiModuloMenuGrupo values (3,20)
 
 select * from cmiEstatus
 
